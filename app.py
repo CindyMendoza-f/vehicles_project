de1501c1939273aa9421d0ef1a2f1fbb7bc86f7c
CindyMendoza-f <cinjmendozf@gmail.com>
2025-03-29 15:45:18 -0500

    commit_app

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+df = pd.read_csv("../vehicles_us.csv")
+
+st.title("Análisis de Datos de Vehículos")
+if st.checkbox("Mostrar datos"): 
+    st.write(df.head())
+
+st.subheader("Distribución de precios")
+fig, ax = plt.subplots()
+df['price'].hist(bins=30, ax=ax)
+st.pyplot(fig)
+
+st.subheader("Relación entre precio y año del vehículo")
+fig, ax = plt.subplots()
+sns.scatterplot(data=df, x='model_year', y='price', alpha=0.5, ax=ax)
+st.pyplot(fig)
+
+def update():
+    st.experimental_rerun()
+st.button("Actualizar", on_click=update)
+
+if st.button("Mostrar gráfico de dispersión"):
+    st.write("Aquí se mostraría un gráfico de dispersión...")
